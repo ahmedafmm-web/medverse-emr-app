5bac941a04716dc43ffa255c44419ebafd62ef03
ahmedafmm-web <ahmed.af.mm@gmail.com>
2026-08-01 14:49:30 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,21 @@
-// تغيير اسم الكاش في كل تعديل يجبر المتصفح يمسح القديم فوراً
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(keys.map(key => caches.delete(key)));
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => caches.delete(key))
+      );
     }).then(() => self.clients.claim())
   );
 });
 
-self.addEventListener('fetch', event => {
-  // يطلب الملفات الجديدة مباشرة من جيت هاب دائماً
-  event.respondWith(fetch(event.request));
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
 });
